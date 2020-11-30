94ebcddc004fd6b388c80522fa35b85d527e9257
dengjiajun666 <1710155943@qq.com>
2020-11-30 19:27:26 +0800

    Initial commit

---
+++ src/main/java/com/djj/git/User.java
@@ -0,0 +1,25 @@
+package com.djj.git;
+
+public class User {
+    private Integer id;
+    private String name;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
